bbe5a918b16a5483c2dfcc5371efcc59d69ea64a
Pedro Almeida <almeida.pedroaugusto25@gmail.com>
2024-10-25 23:25:00 -0300

    fix: alteração na página cadastro

--- src/paginas/cadastro/cadastro.tsx
+++ src/paginas/cadastro/cadastro.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Usuario from '../../models/usuario';
-import './Cadastro.css';
+import './cadastro.css'
 import axios from 'axios';
 
 function Cadastro() {
