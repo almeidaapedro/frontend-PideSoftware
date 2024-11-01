b43c27ce29efd26784dd9ac8aa3b8e17005118a1
PEDRO AUGUSTO SANTOS ALMEIDA <pedro.asalmeida1@senacsp.edu.br>
2024-10-31 21:53:10 -0300

    fix: alterando index

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
         <Navbar/>
           <div className="min-h-[80vh]">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Login />} />
             <Route path='/login' element={<Login />} />
             <Route path='/home' element={<Home />} />
             <Route path='/cadastro' element={<Cadastro />} />
