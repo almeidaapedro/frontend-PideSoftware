90d5dfaf5f0cc0dfe70f859c02fe2fe4978e51c2
Pedro Almeida <almeida.pedroaugusto25@gmail.com>
2024-11-04 10:00:38 -0300

    fix: alterando tela de proprietário

--- src/paginas/proprietario/telaProprietario.tsx
+++ src/paginas/proprietario/telaProprietario.tsx
@@ -6,7 +6,7 @@ import NotificacaoProprietario from './notificacaoProprietario';
 
 const TelaProprietario: React.FC = () => {
   const reservas = [
-    { usuario: "Pedro Almeida", data: "05/11/2024", horario: "19:30" },
+    { usuario: "Pedro Almeida", data: "04/11/2024", horario: "19:30" },
     // Adicione mais reservas conforme necessário
   ];
 
